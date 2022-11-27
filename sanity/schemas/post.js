efcc0eb612db2ae10d622f5f589e5ab492a85416
Kody435 <bittusmaths@gmail.com>
2022-11-27 12:44:47 +0530

    noice

--- sanity/schemas/post.js
+++ sanity/schemas/post.js
@@ -1,65 +1,37 @@
 export default {
-  name: 'post',
-  title: 'Post',
-  type: 'document',
+  name: "movies",
+  title: "Movies",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 96,
       },
     },
     {
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
+      name: "url",
+      title: "URL",
+      type: "string",
     },
     {
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
     },
     {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    },
-    {
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
+      name: "description",
+      title: "Description",
+      type: "string",
     },
   ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const {author} = selection
-      return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`,
-      })
-    },
-  },
-}
+};
--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,10 +5,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
 import category from './category'
 import post from './post'
-import author from './author'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -20,7 +18,6 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     post,
-    author,
     category,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
