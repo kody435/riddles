c2cd42f156c88369ddb0e438e733cc9da9076f39
Kody435 <bittusmaths@gmail.com>
2022-11-28 20:06:17 +0530

    Noice mobile touch

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,19 +15,19 @@ interface Props {
 
 const Home = ({ collections }: Props) => {
   return (
-    <div className='max-w-10xl mx-auto flex flex-col min-h-screen pt-5 pb-20 px-10 2xl:p-0 ' >
+    <div className={styles.main} >
       <Head>
         <title>The OCTULUS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='mb-14 text-4xl font-extralight text-white' >The <span className='font-extrabold'>OCTULUS</span></h1>
+      <h1 className='mb-14 text-4xl font-extralight pl-4 pt-6 text-white' >The <span className='font-extrabold'>OCTULUS</span></h1>
 
-        <div className='Wrapper'>
+        <div className={styles.Wrapper}>
           {collections.map(collection => (
-            <Link href={`/movies/${collection.slug.current}`} className="link">
-            <div className='wrap-div'>
-                  <img className='' src={collection.image} />
-                  <h2 className=''>{collection.title}</h2>
+            <Link href={`/movies/${collection.slug.current}`} className={styles.link}>
+            <div className={styles.wrap_div}>
+                  <img className={styles.img} src={collection.image} />
+                  <h2 className={styles.h2}>{collection.title}</h2>
             </div>
             </Link>
           ))}
