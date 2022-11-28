d474bead52ea0d5f5fd85cc2d6b549e71cd022c8
Kody435 <bittusmaths@gmail.com>
2022-11-28 19:19:00 +0530

    Before CSS Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,18 +14,18 @@ interface Props {
 
 const Home = ({ collections }: Props) => {
   return (
-    <div className='max-w-10xl mx-auto flex flex-col min-h-screen pt-5 pb-20 px-10 2xl:p-0 ' >
+    <div className='max-w-10xl mx-auto flex flex-col min-h-screen pt-5 pb-20 px-2 md:px-10 2xl:p-0 ' >
       <Head>
         <title>The OCTULUS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className='mb-14 text-4xl font-extralight text-white' >The <span className='font-extrabold'>OCTULUS</span></h1>
 
-        <div className='grid lg:grid-cols-7 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-7'>
+        <div className='grid lg:grid-cols-7 w-fit sm:auto-cols-auto md:space-x-4 md:grid-cols-4 xl:grid-cols-7'>
           {collections.map(collection => (
             <Link href={`/movies/${collection.slug.current}`} className=" mb-24">
             <div className=' flex flex-row'>
-                <div className='h-52 w-40 bg-slate-500 '>
+                <div className='h-52 w-40 '>
                   <img className='' src={collection.image} />
                   <h2 className=''>{collection.title}</h2>
               </div>
