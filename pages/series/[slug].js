3ac466c6adc18509114ee6c7aa84d93f3b7031a2
Kody435 <bittusmaths@gmail.com>
2022-12-07 17:13:34 +0530

    Series page style changes

--- pages/series/[slug].js
+++ pages/series/[slug].js
@@ -4,6 +4,7 @@ import { sanityClient } from '../../sanity'
 import { useRouter } from 'next/router'
 import React from 'react';
 import styles from '../../styles/Home.module.css'
+import Link from 'next/link'
 
 const recipeQuery = `*[_type == 'series' && slug.current == $slug][0]{
   title,
@@ -24,7 +25,6 @@ export default function Post({data}) {
 
   return (
     <div className={styles.main}>
-      <iframe src={movie.url} className="w-screen h-screen" allowFullScreen />
       <h1 className="pl-5 text-3xl pt-8 w-screen mr-2 text-white">
         {movie.title}
       </h1>
@@ -37,6 +37,12 @@ export default function Post({data}) {
           <span className="font-extrabold">Release Year</span> :{" "}
           {movie.releaseDate}
         </p>
+      </div>
+
+      <div className='flex flex-row justify-center w-screen p-5'>
+        <Link href="/movies" className='flex p-5 hover:bg-white  rounded-3xl '>
+            <h2 className='text-5xl font-extrabold lg:text-6xl bg-clip-text text-transparent bg-gradient-to-l from-amber-500 to-teal-500'>Watch Now!</h2>
+        </Link>
       </div>
     </div>
   );
