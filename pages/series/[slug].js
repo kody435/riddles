91fad67fc02890de099891149d0d995c058aa44a
Kody435 <bittusmaths@gmail.com>
2022-12-09 09:27:17 +0530

    Green active button

--- pages/series/[slug].js
+++ pages/series/[slug].js
@@ -33,7 +33,7 @@ export default function Post({ data }) {
           <button
             key={oneUrl}
             onClick={() => handleClick(oneUrl)}
-            className="p-4 border-2 rounded-xl"
+            className="p-4 border-2 rounded-xl active:border-green-400"
           >
             {`${index + 1}`}
           </button>
