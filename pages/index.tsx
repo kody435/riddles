65142e318b446dd69a4734892d5d4b078526031a
Cody <bittusmaths@gmail.com>
2023-01-18 07:09:15 +0530

    Revert "Still cleaning up and setting WeaveDB"
    This reverts commit 5fbaf43f33b74bfe8bf3cd82c0f2a3995d0e1f67.

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,9 @@ const Home = () => {
                     <Link href="/movies" className='flex p-2 hover:bg-white  rounded-3xl '>
                         <h2 className='text-5xl p-4 font-extrabold lg:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-amber-400 to-teal-400'>Movies</h2>
                     </Link>
+                    <Link href="/series" className='flex p-2 hover:bg-white rounded-3xl '>
+                        <h2 className='text-5xl p-4 font-extrabold lg:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-blue-400'>Series</h2>
+                    </Link>
                   </div>
             </div>
         </main>
---
+++ pages/series/[slug].js
@@ -0,0 +1,76 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+import { sanityClient } from "../../sanity";
+import React, { useEffect, useState } from "react";
+import styles from "../../styles/Home.module.css";
+
+const recipeQuery = `*[_type == 'series' && slug.current == $slug][0]{
+  title,
+  slug,
+  description,
+  url,
+  image,
+  releaseDate,
+}`;
+
+export default function Post({ data }) {
+  const { serie } = data;
+  const [url, setUrl] = React.useState(serie.url[0]);
+
+  // function to handle the click event of the button and
+  // pass the url to the iframe src
+  const handleClick = async (val) => {
+    setUrl(val);
+  };
+
+  return (
+    <div className={styles.main}>
+      <iframe src={url} className="w-screen h-screen " allowFullScreen />
+      <div className="grid text-white grid-cols-4 md:grid-cols-7 lg:grid-cols-12 space-x-3 pt-5">
+        {serie.url.map((oneUrl, index) => (
+          <button key={oneUrl} onClick={() => handleClick(oneUrl)} className={`p-4 mt-4 border-2 rounded-3xl ${oneUrl===url?"border-green-400":"border-none"}`}>
+            {`${index + 1}`}
+          </button>
+        ))}
+      </div>
+      <h1 className="pl-5 text-3xl pt-8 w-screen mr-2 text-white">
+        {serie.title}
+      </h1>
+      <div className="bg-zinc-800 m-5 rounded-3xl text-white">
+        <p className="pt-6 pb-6 pl-4 pr-4 ">
+          <span className="font-extrabold">The Storyline</span> :{" "}
+          {serie.description}
+          <br></br>
+          <br></br>
+          <span className="font-extrabold">Release Year</span> :{" "}
+          {serie.releaseDate}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = await sanityClient.fetch(
+    `*[_type == "series" && defined(slug.current)]{
+      "params": {
+        "slug": slug.current
+      }
+    }`
+  );
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const serie = await sanityClient.fetch(recipeQuery, { slug });
+  return {
+    props: {
+      data: { serie },
+    },
+  };
+}
