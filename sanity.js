dc9a716a9b860bc5990d3b87a549eebaefb653d6
Cody <bittusmaths@gmail.com>
2023-01-18 06:48:21 +0530

    First commit

--- sanity.js
+++ sanity.js
@@ -1,14 +1,14 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from '@sanity/image-url'
 
-export const config = ({
+export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: "wldop1mm",
   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
-});
+};
 
-const builder = imageUrlBuilder(client);
+const builder = imageUrlBuilder(config);
 
 function urlFor(source) {
   return builder.image(source);
