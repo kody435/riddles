8eaa73348f0bc9169c4eff96a5ad2a54f99235d6
Cody <bittusmaths@gmail.com>
2023-01-18 07:09:20 +0530

    Revert "deletetion of weeds"
    This reverts commit 026df0997bfb623c237ec4aa0c6a9c9532a004e0.

---
+++ sanity/schemas/category.js
@@ -0,0 +1,20 @@
+export default {
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'mainImg',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }
+  ],
+}
