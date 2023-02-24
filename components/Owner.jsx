6a5bb6528920927e5edf744adf40c17e15cd01c4
Cody <bittusmaths@gmail.com>
2023-02-24 17:06:02 +0530

    Bg-Black still not working in App

--- components/Owner.jsx
+++ components/Owner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Owner = () => {
   return (
-    <div className='bg-black text-white flex justify-center mt-10 my-7 text-xl'>Made with ❤️ by Param Patel</div>
+    <div className='bg-black text-white flex justify-center pt-10 py-7 text-xl'>Made with ❤️ by Param Patel</div>
   )
 }
 
