f4de246bbf1482b262bbe2408a3d8c76fbff29dc
Cody <bittusmaths@gmail.com>
2023-02-24 17:00:31 +0530

    Some changes for making Owner.jsx visible on App

--- components/Owner.jsx
+++ components/Owner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Owner = () => {
   return (
-    <div className='text-white flex justify-center mt-10 my-7 text-xl'>Made with ❤️ by Param Patel</div>
+    <div className='bg-black text-white flex justify-center mt-10 my-7 text-xl'>Made with ❤️ by Param Patel</div>
   )
 }
 
