8d281086b4b9c065fb48461d4e39296500122bfd
Cody <bittusmaths@gmail.com>
2023-03-01 04:59:10 +0530

    Correction in stylings

--- components/Owner.jsx
+++ components/Owner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Owner = () => {
   return (
-    <div className='bg-black text-white flex justify-center pt-10 py-7 text-xl'>Made with ❤️ by Param Patel</div>
+    <div className='bg-black text-white flex justify-center pt-10 py-7 text-lg md:text-xl'>Made with ❤️ by Param Patel</div>
   )
 }
 
