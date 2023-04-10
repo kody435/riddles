7ecea47111c5b3f1bc4a09fe2c53db906231c255
Cody <bittusmaths@gmail.com>
2023-04-10 17:10:07 +0530

    Landing page optimized for mobiles

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,46 +26,48 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="h-screen bg-white text-black flex items-center ">
+      <main className="h-screen bg-black text-white flex items-center ">
         <div className="">
-        {/* Header start */}
+          {/* Header start */}
           <div className="grid grid-cols-1 lg:grid-cols-2 pt-32 lg:pt-0 ">
             {/* LEFT header */}
-            <div className="flex items-center justify-center mt-12">
-              <div className="shadow-lg rounded-lg">
-                <div className="text-center">
-                  <Image
-                    alt=""
-                    className="rounded-xl"
-                    src="https://image.tmdb.org/t/p/w300/bOFaAXmWWXC3Rbv4u4uM9ZSzRXP.jpg"
-                    loading="lazy"
-                    width={300}
-                    height={450}
-                  />
-                  <h3 className="text-white font-bolder text-md  ">
-                    Fast and Furious 9
-                  </h3>
+            <div className="">
+              <div className="flex items-center justify-center mt-12 lg:mt-0">
+                <div className="shadow-lg rounded-lg">
+                  <div className="text-center">
+                    <Image
+                      alt=""
+                      className="rounded-xl"
+                      src="https://image.tmdb.org/t/p/w300/bOFaAXmWWXC3Rbv4u4uM9ZSzRXP.jpg"
+                      loading="lazy"
+                      width={300}
+                      height={450}
+                    />
+                    {/* <h3 className="text-white font-bolder text-md ">
+                          Fast and Furious 9
+                        </h3> 
+                    */}
+                  </div>
                 </div>
               </div>
             </div>
 
             {/* RIGHT header */}
-
-            <div className="flex flex-col items-center justify-center gap-4">
+            <div className="flex flex-col items-center justify-center gap-4 mt-5 lg:mt-0 text-black bg-green-300 pt-8 lg:pt-0 lg:rounded-l-3xl  rounded-none">
               <h1 className="text-5xl md:text-6xl lg:text-7xl text-center lg:text-left font-bold scrollbar-none">
                 Watch your<br></br> Favourite Movies <br></br> & TV Shows
               </h1>
-              <h3 className="text-center lg:text-left mx-20 text-gray-700 ">
+              <h3 className="text-center lg:text-left mx-20 text-black">
                 Explore a vast library of top-rated films and binge-worthy
                 series, all at your fingertips. Dive into countless genres and
                 enjoy seamless streaming, tailored to your unique taste. Start
                 your next adventure in entertainment today.
               </h3>
 
-              <div className="flex flex-row mb-10 mt-5">
+              <div className="flex flex-row mb-10 lg:mb-0 mt-10">
                 <Link
                   href="/movies"
-                  className="flex justify-center items-left text-white bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-50% to-emerald-500 to-90% px-7 py-3 font-bold rounded-full"
+                  className="flex justify-center items-left text-xl text-white bg-black px-7 py-3 font-bold rounded-full "
                 >
                   Play Now
                 </Link>
@@ -74,8 +76,6 @@ const Home = () => {
           </div>
         </div>
         {/* Header finish */}
-
-
       </main>
     </div>
   );
